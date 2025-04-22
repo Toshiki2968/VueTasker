5c69a55f89404fd24ca188575f2900679bae14a5
t-sumiya-uf <tsk2968@gmail.com>
2025-04-22 23:47:56 +0900

    add: フロントエンドを追加

---
+++ frontend/src/lib/axios.js
@@ -0,0 +1,11 @@
+// src/lib/axios.js
+import axios from 'axios';
+
+const api = axios.create({
+  baseURL: 'http://localhost:8000/api', // LaravelのAPIエンドポイント
+  headers: {
+    'Content-Type': 'application/json',
+  },
+});
+
+export default api;
---
+++ frontend/src/pages/TaskList.vue
@@ -0,0 +1,23 @@
+<template>
+    <div>
+      <h1>タスク一覧</h1>
+      <ul>
+        <li v-for="task in tasks" :key="task.id">
+          {{ task.title }} - {{ task.description }}
+        </li>
+      </ul>
+    </div>
+  </template>
+  
+  <script setup>
+  import { onMounted, ref } from 'vue';
+  import api from '@/lib/axios';
+  
+  const tasks = ref([]);
+  
+  onMounted(async () => {
+    const response = await api.get('/tasks');
+    tasks.value = response.data;
+  });
+  </script>
+  
